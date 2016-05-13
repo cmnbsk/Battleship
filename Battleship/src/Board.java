7ff9a979d675ec39430962ac2a026cc4f0168965
darkbluegt <darkbluegt@gmail.com>
2016-05-13 20:07:31 +0200

    Deleted Ship.java and modificated Board.java (shoot and isDestroyed methods)

--- Battleship/src/Board.java
+++ Battleship/src/Board.java
@@ -7,4 +7,21 @@ public class Board {
 	Board(){
 		board = new boolean[10][10];
 	}
+	
+	public boolean shoot(int x, int y){
+		if(board[x][y]==true){
+			board[x][y]=false;
+			return true;
+		}
+		else return false;		
+	}
+	public boolean isShipDestroyed(int x, int y){
+		if(board[x-1][y+1]==true || board[x-1][y]==true || board[x-1][y+1]==true 
+				|| board[x][y+1]==true || board[x][y-1]==true ||
+				board[x+1][y+1]==true || board[x+1][y]==true || board[x+1][y-1]==true){
+			return false;
+		}
+		else return true;
+			
+	}
 }
