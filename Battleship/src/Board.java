de6242e782905183c8d1f430ed78e2daf299fed3
darkbluegt <darkbluegt@gmail.com>
2016-05-13 21:20:27 +0200

    Added addShip method to Board

--- Battleship/src/Board.java
+++ Battleship/src/Board.java
@@ -8,20 +8,50 @@ public class Board {
 		board = new boolean[10][10];
 	}
 	
-	public boolean shoot(int x, int y){
+	public int shoot(int x, int y){			//0-pud³o, 1-tylko trafiony, 2-trafiony i zatopiony
 		if(board[x][y]==true){
 			board[x][y]=false;
-			return true;
+			if(isShipDestroyed(x,y))	return 2;
+			else	return 1;
 		}
-		else return false;		
+		else	return 0;		
 	}
-	public boolean isShipDestroyed(int x, int y){
+				
+	private boolean isShipDestroyed(int x, int y){	//true-zatopiony false-niezatopiony
 		if(board[x-1][y+1]==true || board[x-1][y]==true || board[x-1][y+1]==true 
 				|| board[x][y+1]==true || board[x][y-1]==true ||
 				board[x+1][y+1]==true || board[x+1][y]==true || board[x+1][y-1]==true){
 			return false;
 		}
 		else return true;
-			
 	}
+	
+	public void addShip(int x, int y){ //add single ship
+		board[x][y]=true;		
+	}
+	
+	public void addShip(int firstX, int firstY, int lastX, int lastY){  //add multiple ship
+		if(firstX>lastX){  	//sortowanie wspo³rzêdnych X
+			int temp = firstX;
+			firstX = lastX;
+			lastX = temp;
+		}
+		if(firstY>lastY){  	//sortowanie wspo³rzêdnych Y
+			int temp = firstY;
+			firstY = lastY;
+			lastY = temp;
+		}
+		if(firstX==lastX){ 	//dziêki temu wiemy ¿e statek jest postawiony pionowo (sta³a X)
+			for(int i=firstY; i<=lastY; i++)
+								board[firstX][i]=true;
+		}
+		else if(firstY==lastY){ //statek jest postawiony poziomo (sta³a Y)
+			for(int i=firstX; i<=lastX; i++)
+								board[i][firstY]=true;
+		}
+		else System.out.println("Ship can not be situated diagonally!");		
+	}
+	
+	
+	
 }
