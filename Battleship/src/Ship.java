ce74f2abe51ab0bcb9d0d83938e4071939775f99
darkbluegt <darkbluegt@gmail.com>
2016-05-19 09:18:26 +0200

    Update Ship.java

--- Battleship/src/Ship.java
+++ Battleship/src/Ship.java
@@ -17,17 +17,30 @@ public class Ship extends Board{
 	Ship(int[] coordinates){
 		if(isVertically(coordinates)){
 			this.size=coordinates[3]-coordinates[1]+1;
-			for(int i=0; i<this.size; i++)
+ship_id = new int [this.size];
+int j =0;
+			for(int i=0; i<this.size; i++){
 				board[coordinates[0]][coordinates[1]+i]=1;
-		}
+ship_id[j]=coordinates[0];
+ship_id[j+1]=coordinates[1]+i;
+j+=2;
+
+}
+}
 		else{
 			this.size=coordinates[2]-coordinates[0]+1;
-			for(int i=0; i<this.size; i++)
+
+ship_id = new int [this.size];
+int j =0;
+			for(int i=0; i<this.size; i++){
 				board[coordinates[0]+i][coordinates[1]]=1;
-		}
-		ship_id = new int [2];
-		ship_id[0]=coordinates[0];
-		ship_id[1]=coordinates[1];
+ship_id[j]=coordinates[0]+i;
+ship_id[j+1]=coordinates[1];
+j+=2;
+
+} }
+
+
 	}
 	
 	boolean isDestroyed(){
@@ -38,9 +51,10 @@ public class Ship extends Board{
 	
 	static Ship findShip(int x, int y){		
 		for( Ship s : ships){
-			if(s.ship_id[0]==x && s.ship_id[1]==y)
+for (int i=0; i <ship_id.length; i+=2){
+			if(s.ship_id[i]==x && s.ship_id[i+1]==y)
 				return s;			
-		}
+		} }
 		return null;
 	}
 }
